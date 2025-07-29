ff7cd2e8aa1cf76054c7a7505ff81cc707edfed8
Anluhen <andrehenc@gmail.com>
2025-07-29 08:57:02 -0300

    feat(App.jsx): Hide form when submitted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import PersonalInfoForm from './components/PersonalInfoForm.jsx'
+import EducationForm from './components/EducationForm.jsx'
 
 function App() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    school: '',
+    title: '',
+    date: '',
   });
   const [submittedData, setSubmittedData] = useState(null);
   const [errors, setErrors] = useState({});
@@ -35,23 +39,32 @@ function App() {
 
   return (
     <div>
-      <h1>CV Application</h1>
-      <PersonalInfoForm
-        formData={formData}
-        setFormData={setFormData}
-        onSubmit={handleSubmit}
-        errors={errors}
-        submitted={submitted}
-      />
-      {/* // A section to add your educational experience (school name, title of study and date of study) */}
-      {/* <EducationForm /> */}
-      {/* // A section to add practical experience (company name, position title, main responsibilities of your jobs, date from and until when you worked for that company) */}
-      {/* <ExperienceForm /> */}
-      {/* // Be sure to include an edit and submit button for each section or for the whole CV. The submit button should submit your form and display the value of your input fields in HTML elements. The edit button should add back (display) the input fields, with the previously displayed information as values. In those input fields, you should be able to edit and resubmit the content. You’re going to make heavy use of state and props, so make sure you understood those concepts.
+      {!submittedData && (
+        <div style={{ marginTop: '2rem' }}>
+          <h1>CV Application</h1>
+          <PersonalInfoForm
+            formData={formData}
+            setFormData={setFormData}
+            onSubmit={handleSubmit}
+            errors={errors}
+            submitted={submitted}
+          />
+          {/* // A section to add your educational experience (school name, title of study and date of study) */}
+           {/*<EducationForm
+            formData={formData}
+            setFormData={setFormData}
+            onSubmit={handleSubmit}
+            errors={errors}
+            submitted={submitted}
+          />*/}
+          {/* // A section to add practical experience (company name, position title, main responsibilities of your jobs, date from and until when you worked for that company) */}
+          {/* <ExperienceForm /> */}
+          {/* // Be sure to include an edit and submit button for each section or for the whole CV. The submit button should submit your form and display the value of your input fields in HTML elements. The edit button should add back (display) the input fields, with the previously displayed information as values. In those input fields, you should be able to edit and resubmit the content. You’re going to make heavy use of state and props, so make sure you understood those concepts.
       // Create a components directory under your src directory and add your components.
       // Include a styles directory under your src directory for your CSS files. You’ll need to import these in the component files to use them. */}
-      <hr />
-
+          <hr />
+        </div>
+      )}
       {submittedData && (
         <div style={{ marginTop: '2rem' }}>
           <h2>Submitted Data:</h2>
